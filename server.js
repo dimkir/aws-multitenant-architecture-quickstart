49265ad9510d01abfc726720b506eca49bdde3c3
Dimitry Kireyenkov <dimkir@gmail.com>
2018-11-03 12:23:33 +0000

    adds entry point into express application

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 //  Express
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const RequestDebug = require('request-debug');
+RequestDebug(request);
+
 
 var app = express();
 
@@ -15,7 +19,16 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-    next();
+    // intercept OPTIONS method
+    console.log(`request.method = ` +  req.method);
+    if ('OPTIONS' == req.method) {
+        console.log(`Sending 200 for options`);
+        res.send(200);
+    }
+    else {
+        next();
+    }    
+    // next();
 });
 
 
@@ -27,9 +40,38 @@ var configuration = configModule.configure(process.env.NODE_ENV);
 //Configure Logging
 const winston = require('winston');
 winston.level = configuration.loglevel;
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `combined.log` 
+      // - Write all logs error (and below) to `error.log`.
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+  
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  // 
+// if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+     format: winston.format.simple(),
+     level: 'debug',
+    }));
+// }
+
+winston.add(logger);
+// TODO: add winston engine
 
 
 require('./functions/auth-manager')({app, configuration, winston});
+require('./functions/user-manager')({app, configuration, winston});
+require('./functions/tenant-manager')({app, configuration, winston});
+require('./functions/system-registration')({app, configuration, winston, request});
 
 
 module.exports = app;
